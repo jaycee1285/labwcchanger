694cfea95f410fe70a824ce483fc59b660430407
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-28 13:10:52 -0400

    gdit

--- flake.nix
+++ flake.nix
@@ -2,30 +2,27 @@
   description = "LabWC theme changer (Flutter)";
 
   inputs = {
-    nixpkgs     .url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils .url = "github:numtide/flake-utils";
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.flutter.buildFlutterApplication rec {
-          pname   = "lchanger";
-          version = "0.1.0";
+      in {
+        packages.default =
+          (pkgs.flutter.buildFlutterApplication rec {
+            pname        = "lchanger";
+            version      = "0.1.0";
+            src          = ./.;
+            pubspecLock  = ./pubspec.lock;
+          }) // {
+            etc = { };
+          };
 
-          src = ./.;
-
-          # 👉  Use a *path literal*, not a string:
-          pubspecLock = ./pubspec.lock;
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.flutter pkgs.dart ];
         };
-        {
-    # Make /etc contribution empty so the merge code is happy
-    etc = { };
-  };
-
-        devShell = pkgs.mkShell { buildInputs = [ pkgs.flutter pkgs.dart ]; };
       });
 }
