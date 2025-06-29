e618396de13e16c123077e3278ecb546d4c3b798
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-29 13:20:06 -0400

    sdfsdf

--- flake.nix
+++ flake.nix
@@ -1,60 +1,55 @@
+# flake.nix
 {
   description = "LabWC theme changer (Flutter)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs     .url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils .url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        
-        app = pkgs.stdenv.mkDerivation rec {
-          pname = "labwcchanger";
-          version = "0.1";
-          
+
+        app = pkgs.stdenv.mkDerivation {
+          pname    = "labwcchanger";
+          version  = "0.1";
+
+          ## 1 – point directly at the release asset
           src = pkgs.fetchurl {
-            url = "https://github.com/jaycee1285/labwcchanger/releases/download/${version}/labwcchanger";
+            url    = "https://github.com/jaycee1285/labwcchanger/releases/download/relase/labwcchanger";
+            # put lib.fakeSha256 while iterating; replace with real hash once Nix prints it
             sha256 = "sha256-VoYIgTq+wYBFx73mFSF9x9R24K0uYbcWgcP+VfGgWPw=";
           };
-          
+
+          ## 2 – no unpack step, so skip to install
+          phases = [ "installPhase" ];
           dontUnpack = true;
-          
-          nativeBuildInputs = with pkgs; [ 
-            autoPatchelfHook 
-          ];
-          
+
+          ## 3 – let autoPatchelfHook patch ELF RPATHs
+          nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+
+          ## 4 – runtime libraries Flutter-on-Linux typically needs
           buildInputs = with pkgs; [
-            gtk3
-            libxkbcommon
-            xorg.libX11
-            xorg.libXext
-            xorg.libXi
-            xorg.libXrender
-            xorg.libXtst
-            libGL
-            stdenv.cc.cc.lib
-            glib
-            pango
-            cairo
-            gdk-pixbuf
-            atk
-            wayland
-            libepoxy
+            gtk3 libxkbcommon
+            xorg.libX11 xorg.libXext xorg.libXi xorg.libXrender xorg.libXtst
+            libGL       # OpenGL
+            glib pango cairo gdk-pixbuf atk
+            wayland libepoxy
           ];
-          
+
           installPhase = ''
-            mkdir -p $out/bin
-            cp $src $out/bin/labwcchanger
+            runHook preInstall
+            install -D $src $out/bin/labwcchanger
             chmod +x $out/bin/labwcchanger
+            runHook postInstall
           '';
         };
-        
       in {
         packages.default = app;
 
+        ## (optional) dev shell for hacking on Flutter code
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [ flutter dart ];
         };
