cf857ac58b03251f273406390449195c348ca416
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-09-09 16:07:47 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,97 +8,113 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { 
+          inherit system;
+          config.allowUnfree = true;  # Needed for some graphics drivers
+        };
         lib = pkgs.lib;
         
         bundle = builtins.fetchTarball {
-          url = "https://github.com/jaycee1285/labwcchanger/releases/download/release/labwcchanger1.0.tar.gz";
-          sha256 = "sha256:125b6snri611j31qlp4hqr9056gd14g2z70hi09q4pkkq4gj5754";
+          url = "https://github.com/jaycee1285/labwcchanger/releases/download/release/labwcchanger-1.1.tar.gz";
+          sha256 = "sha256:f819494b44291a6ec042e0558d1e2021d69b164ebdc086d1c0925734a4bd9148";
         };
-        
-        runtimeLibs = with pkgs; [
-          gtk3
-          libxkbcommon
-          xorg.libX11
-          xorg.libXext
-          xorg.libXi
-          xorg.libXrender
-          xorg.libXtst
-          libGL
-          glib
-          pango
-          cairo
-          gdk-pixbuf
-          atk
-          at-spi2-atk  # Added for ATK bridge
-          wayland
-          libepoxy
-          zlib
-          harfbuzz
-          stdenv.cc.cc.lib  # for libstdc++.so.6
-          mesa  # Added for GL implementation
-        ];
       in
       {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "labwcchanger";
-          version = "1.0";
+          version = "1.1";
           src = bundle;
           
           nativeBuildInputs = with pkgs; [
-            autoPatchelfHook  # Use autoPatchelfHook instead of manual patchelf
+            autoPatchelfHook
             makeWrapper
-            wrapGAppsHook  # Important for GTK apps
+            wrapGAppsHook
           ];
           
-          buildInputs = runtimeLibs;
+          buildInputs = with pkgs; [
+            # Core GTK/GDK dependencies
+            gtk3
+            glib
+            pango
+            cairo
+            gdk-pixbuf
+            atk
+            at-spi2-atk
+            
+            # X11/Wayland dependencies
+            xorg.libX11
+            xorg.libXext
+            xorg.libXi
+            xorg.libXrender
+            xorg.libXtst
+            libxkbcommon
+            wayland
+            
+            # Critical: OpenGL dependencies
+            libGL
+            libGLU
+            mesa
+            mesa.drivers
+            libepoxy
+            
+            # Video/Graphics
+            libdrm
+            
+            # Other runtime dependencies
+            zlib
+            harfbuzz
+            stdenv.cc.cc.lib
+          ];
           
-          # Tell autoPatchelfHook to also look in the bundled lib directory
-          appendRunpaths = [ "$out/share/labwcchanger/lib" ];
+          # Tell autoPatchelfHook where to find Flutter's bundled libs
+          appendRunpaths = [ 
+            "$out/share/labwcchanger/lib"
+            "${pkgs.mesa}/lib"
+            "${pkgs.libGL}/lib"
+          ];
           
-          # Don't strip the binaries as it might break Flutter
           dontStrip = true;
+          dontWrapGApps = true;  # We'll do it manually
           
           installPhase = ''
             runHook preInstall
             
-            # Create the proper directory structure
             mkdir -p $out/share/labwcchanger
             cp -r . $out/share/labwcchanger/
-            
-            # Make sure the binary is writable for patching
             chmod -R +w $out/share/labwcchanger
             
-            # The autoPatchelfHook will handle the patching automatically
-            # But we need to ensure it looks in the right place for Flutter libs
-            
-            # Create wrapper that sets up the environment properly
-            mkdir -p $out/bin
-            makeWrapper $out/share/labwcchanger/labwcchanger $out/bin/labwcchanger \
-              --chdir $out/share/labwcchanger \
-              --prefix LD_LIBRARY_PATH : "$out/share/labwcchanger/lib:${lib.makeLibraryPath runtimeLibs}" \
-              --set GDK_GL "gles"  # Sometimes helps with GL issues
-              
             runHook postInstall
           '';
           
-          # This helps autoPatchelfHook find the bundled Flutter libraries
           postFixup = ''
-            # Ensure the main binary can find its bundled libraries
-            patchelf --add-rpath '$ORIGIN/lib' $out/share/labwcchanger/labwcchanger
+            # Manually wrap with GApps and additional environment
+            wrapProgram $out/share/labwcchanger/labwcchanger \
+              ''${gappsWrapperArgs[@]} \
+              --chdir $out/share/labwcchanger \
+              --prefix LD_LIBRARY_PATH : "$out/share/labwcchanger/lib:${lib.makeLibraryPath buildInputs}" \
+              --set-default LIBGL_ALWAYS_SOFTWARE 1 \
+              --set-default GDK_BACKEND x11
             
-            # Patch any plugin libraries that might need system libraries
-            for lib in $out/share/labwcchanger/lib/*.so; do
-              if [ -f "$lib" ]; then
-                patchelf --add-rpath "${lib.makeLibraryPath runtimeLibs}" "$lib" || true
-              fi
-            done
+            # Create the bin wrapper
+            mkdir -p $out/bin
+            ln -s $out/share/labwcchanger/labwcchanger $out/bin/labwcchanger
           '';
         };
         
+        # Development shell with Flutter
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ flutter dart ];
+          buildInputs = with pkgs; [ 
+            flutter 
+            dart
+            pkg-config
+            gtk3
+            libGL
+          ];
+          
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.libGL}/lib:${pkgs.mesa}/lib:$LD_LIBRARY_PATH"
+          '';
         };
-      }
+      };
     );
 }
